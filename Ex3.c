ce4bcce92e0b18443df6647196bff614035b6cf8
wuttiwong <wutkmutt@hotmail.com>
2018-07-23 22:15:21 +0700

    Success Exam2.c

--- Ex3.c
+++ Ex3.c
@@ -1,68 +1,29 @@
 #include<stdio.h>
 #include<math.h>
 float calPrice(float);
-float calPrice1(float);
-float calPrice2(float);
-float calPrice3(float);
 
 int main(){
 	
 	float distance = 0.00;
-	float price = 35.00;
-	
 	
 	printf("Enter distance :");
 	scanf("%f",&distance);
 	
-	printf("%.2f",calPrice(distance));
-	
-	
+	printf("%.2f Bath",calPrice(distance));
 	
 	return 0;
 }
-float calPrice1(float dis){
-	float totalPrice = 0.00;
-	float newDis = 0.00;
-	
-		newDis = dis - 2.00;
-		
-		totalPrice = newDis * 4.50;
-
-	return totalPrice;
-}
-
-float calPrice2(float dis){
-	float totalPrice = 0.00;
-	float newDis = 0.00;
-
-		newDis = dis - 12.00;
-		
-		totalPrice = newDis * 4.50;
-	
-	return totalPrice;
-}
-float calPrice3(float dis){
-	float totalPrice = 0.00;
-	float newDis = 0.00;
-	
-		newDis = dis - 20.00;
-		
-		totalPrice = newDis * 5.50;
-
-	return totalPrice;
-}
 float calPrice(float dis){
-	float totalPrice = 35.00;
-	if(dis <= 2.00){
-		totalPrice = totalPrice;
-	}else if(dis <= 12.00){
-		totalPrice = totalPrice + calPrice1(dis);
+	float totalPrice = 0.00;
+	if(dis <= 12.00){
+		totalPrice = ( (dis - 2.00) * 4.50 ) + 35.00;
 	}else if(dis <= 20.00){
-			totalPrice = totalPrice + calPrice1(dis) + calPrice2(dis);
+		totalPrice = ( (dis - 12.00 ) * 5.00 ) + ( (10.00) * 4.50 ) + 35.00;
 	}else{
-			totalPrice = totalPrice + calPrice1(dis) + calPrice2(dis) + calPrice3(dis);
+		totalPrice = ( ( dis - 20.00 ) * 5.50 ) + ( (8.00 ) * 5.00 ) + ( (10.00) * 4.50 ) + 35.00;
 	}
-
+	totalPrice = (int)totalPrice % 2 == 0 ? totalPrice-1:totalPrice;
 	return floor(totalPrice);
 }
+
 
